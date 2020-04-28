c0f18be97c2763e26c9244a9a221db5de32673ef
eliasortega04 <eliaspotter2marzo@gmail.com>
2020-04-28 05:48:28 +0000

    Elias 28/04/20: Creación y asignación de estructuras y trabajo para ambos

--- main.c
+++ main.c
@@ -1,37 +1,31 @@
+/* ********************* TEXTO SIN ACENTOS *********************/
+// Codigo programado con exito en Dev C++
+
 #include <stdio.h>
 #include <stdlib.h>
+#define T 20
+
+struct rgstr {
+	char user[T - 5];
+	char psswrd[T - 10];
+}; // Estructura de inicio de sesion
+
+struct order {
+	char name[T];
+	unsigned int nmbr_order;
+	unsigned int blldng;
+	unsigned int clssrm;
+	float dscnt;
+	float sbttl;
+	float ttl;
+}; // Estructura de pedido
 
 int main() {
+	// Elias: Switch inicio y registro de sesión, con funciones y apuntadores, vriables de estructura.
+	// tambien arreglos Usar system("cls"); en Dev C++ para limpiar pantalla 
+	// Mon: Switch para pedido, con funciones, con arreglos, variables de esructura.
 
-printf("\n\t >>>>>>>>>><<<<<<<<<< >>>>><<<<	   >>>>>>>>>>>   >>>>><<<<<   >>>>>>>>>>  >>>>>>>>>>   >>          <<   \n");
-printf("\t          ><          >>     <<    >>            >>      <<   >>      >>  >>      >>    >>        <<    \n");
-printf("\t          ><          >>     <<    >>            >>      <<   >>      >>  >>      >>     >>      <<     \n");
-printf("\t          >< 	      >>     <<    >>            >>      <<   >>      >>  >>      >>      >>    <<      \n");
-printf("\t          >< 	      >>	 <<    >>            >>      <<   >>      >>  >>      >>       >>  <<       \n");
-printf("\t          >< 	        >>     <<    >>            >>>>><<<<<   >>>>>>>>>>  >>>>>>>>>>        >><<        \n");
-printf("\t          ><			>>     <<    >>            >>      <<   >>>>>       >>>>>              ><         \n");
-printf("\t          >< 			>>     <<    >>            >>      <<   >>  >>>     >>  >>>            ><         \n");
-printf("\t          >< 		    >>     <<    >>            >>      <<   >>     >>>  >>     >>>         ><         \n");
-printf("\t          >< 		    >>>>><<<<    >>>>>>>>>>>   >>      <<   >>      >>  >>      >>         ><         \n");
+  //Elias 28/04/20: Creación y asignación de estructuras y trabajo para ambos
 
-printf("\n\t\t\t   	>>>>><<<<<   >>>>>><<<<<<   >>>>>><<<<<<	\n");
-printf("\t\t\t 	>>       <<   >>            >>\n");
-printf("\t\t\t 	>>	     <<   >>            >>\n");
-printf("\t\t\t 	>>       <<   >>            >>\n");
-printf("\t\t\t 	>>	     <<   >>            >> \n");
-printf("\t\t\t 	>>	     <<   >>>>><<<<<<   >>>>>><<<<<<\n");
-printf("\t\t\t 	>>	     <<   >>            >>\n");
-printf("\t\t\t    >>       <<   >>            >>\n");
-printf("\t\t\t    >>  	 <<   >>            >> \n");
-printf("\t\t\t    >>>>>><<<<<   >>           >>\n");
-
-
-int game;
-printf("\n\n\t\t 1.- ");
-printf("\t\t 2.- ");
-printf("\t\t 3.- ");
-printf("\t\t 4.- ");
-printf("\t\t 5.- ");
-printf("\n\n\n\n\t\t Seleccione ... ");
-  return 0;
+	return 0;
 }
